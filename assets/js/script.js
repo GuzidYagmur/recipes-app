1d142bc9b690154211f59f63f4f6a3812c39a2de
onur han hacimustafaoglu <onurhanhacimustafaoglu@gmail.com>
2025-02-03 00:08:59 +0300

    async js added

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,46 +1,48 @@
 const foodMenus = document.querySelector(".foodMenu");
 let recipes = [];
-fetch("https://dummyjson.com/recipes").then(res => {
-    if(res.ok){
-        return res.json()
-    }
-}).then(data => {
-    recipes = [
-        ...data.recipes
-    ]
-    for (const recipe of data.recipes) {
-        const template = productTemplate(recipe);
-        foodMenus.innerHTML += template;
-    }
-}).catch(e => {
-    foodMenus.innerHTML = "Bir Hata Oluştu :/"
-})
 
-// const a = null;
-// const image = a ?? "https://placehold.co/320x240"
+async function fetchRecipes() {
+    try {
+        const res = await fetch("https://dummyjson.com/recipes");
+        if (!res.ok) {
+            throw new Error("Veri alınırken hata oluştu");
+        }
 
-function productTemplate({ id, name, image, caloriesPerServing, cookTimeMinutes }){
+        const data = await res.json();
+        recipes = [...data.recipes];
+
+        for (const recipe of data.recipes) {
+            const template = productTemplate(recipe);
+            foodMenus.innerHTML += template;
+        }
+    } catch (error) {
+        foodMenus.innerHTML = "Bir Hata Oluştu :/";
+        console.error(error);
+    }
+}
+
+fetchRecipes();
+
+function productTemplate({ id, name, image, caloriesPerServing, cookTimeMinutes }) {
     return `
-                        <div class="menu-content">
-                    <div class="img-head">
-                        <img src="${image}" alt="">
-                        <div class="food-head">
-                            <h4>${name}</h4>
-                            <i class="fa-regular fa-heart"></i>
-                        </div>
-                    </div>
-                    <div class="kcal-min">
-                        <div class="kcal">
-                            <i class="fa-sharp fa-solid fa-fire"></i>
-                            <p>${caloriesPerServing} Kcal  ·</p>
-                        </div>
-                        <div class="min">
-                            <i class="fa-regular fa-clock"></i>
-                            <p>${cookTimeMinutes} min</p>
-                        </div>
-                    </div>
-    
-    
+        <div class="menu-content">
+            <div class="img-head">
+                <img src="${image}" alt="">
+                <div class="food-head">
+                    <h4>${name}</h4>
+                    <i class="fa-regular fa-heart"></i>
                 </div>
-    `
+            </div>
+            <div class="kcal-min">
+                <div class="kcal">
+                    <i class="fa-sharp fa-solid fa-fire"></i>
+                    <p>${caloriesPerServing} Kcal  ·</p>
+                </div>
+                <div class="min">
+                    <i class="fa-regular fa-clock"></i>
+                    <p>${cookTimeMinutes} min</p>
+                </div>
+            </div>
+        </div>
+    `;
 }
